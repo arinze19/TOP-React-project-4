559143ef33d5213c96d420dd2eb3157a37a87597
Arinze <arinze19@outlook.com>
2021-12-19 18:03:55 +0100

    style adjustments

--- src/components/reviews/ReviewList.js
+++ src/components/reviews/ReviewList.js
@@ -4,10 +4,15 @@ import '../../styles/reviews/review-list.css';
 
 export default function ReviewList({ comments }) {
   return (
-    <ul className='review-list-container'>
-      {comments.map((comment, index) => (
-        <ReviewItem key={index} comment={comment} />
-      ))}
-    </ul>
+    <>
+      <h2 style={{textAlign: 'center', margin: '2rem'}}>Here's what some our enantiomers have to say</h2>
+      <br />
+      <br />
+      <ul className='review-list-container'>
+        {comments.map((comment, index) => (
+          <ReviewItem key={index} comment={comment} />
+        ))}
+      </ul>
+    </>
   );
 }
