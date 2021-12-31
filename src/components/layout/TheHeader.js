2ece9feb60e9ceacd9243ab5e7203b046f7722b8
Arinze <arinze19@outlook.com>
2021-12-31 13:46:55 +0100

    add auth page link

--- src/components/layout/TheHeader.js
+++ src/components/layout/TheHeader.js
@@ -1,40 +1,45 @@
-import "../../styles/layout/the-header.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import debounce from "../../helpers/debounce";
+import '../../styles/layout/the-header.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import debounce from '../../helpers/debounce';
 import { useSelector, useDispatch } from 'react-redux';
 
 function TheHeader() {
-  const { cart } = useSelector(store => store);
+  const { cart } = useSelector((store) => store);
   const dispatch = useDispatch();
 
   const [yOffset, setYOffset] = useState(0);
-  const addBackground         = { backgroundColor: "#fff" };
-  const removeBackground      = { backgroundColor: "transparent" };
+  const addBackground = { backgroundColor: '#fff' };
+  const removeBackground = { backgroundColor: 'transparent' };
 
   const checkSlide = () => {
     if (window.scrollY > 10) return setYOffset(1);
     return setYOffset(0);
   };
 
-  window.addEventListener("scroll", debounce(checkSlide));
+  window.addEventListener('scroll', debounce(checkSlide));
 
   return (
     <div
-      className="header-container"
+      className='header-container'
       style={yOffset !== 0 ? addBackground : removeBackground}
     >
-      <div className="header-container__motto">
-        <p>Comfort</p>
-        <p>For</p>
-        <p>You</p>
+      <div className='header-container__motto'>
+        <Link to='/auth'>
+          <i className='las la-user'></i>
+        </Link>
       </div>
 
-      <div className="header-container__logo">
-          <Link to="/"><h2>Octane.</h2></Link>
+      <div className='header-container__logo'>
+        <Link to='/'>
+          <h2>Octane.</h2>
+        </Link>
       </div>
 
-      <div className="header-container__cart" onClick={() => dispatch({ type: 'TOGGLE_CART' })}>
+      <div
+        className='header-container__cart'
+        onClick={() => dispatch({ type: 'TOGGLE_CART' })}
+      >
         <p>Cart({cart.length})</p>
       </div>
     </div>
--- src/components/pages/AuthPage.js
+++ src/components/pages/AuthPage.js
@@ -2,19 +2,45 @@ import React from 'react';
 import '../../styles/pages/auth-page.css';
 
 export default function AuthPage() {
+
+
   return (
     <div className='auth-container'>
-      <div className='auth-log-in'>
-        <h2>Login</h2>
-        <form className='auth-log-in__form-control'>
-          <label htmlFor='email'>Email:</label>
-          <input type='text' name='email' />
-          <label htmlFor='password'>Password:</label>
-          <input type='password' />
-          <button className="btn">sign in</button>
-        </form>
-      </div>
-      <div className='auth-sign-up'></div>
+      <main className="auth-container__main">
+        <div className='auth-container__main__login'>
+          <h2>Login</h2>
+          <form className='auth-container__form-control'>
+            <label htmlFor='email'>Email:</label> <br />
+            <input type='text' name='email' /> 
+            <br />
+            <br />
+            <label htmlFor='name'>Name:</label> <br />
+            <input type='text' name='name' /> 
+            <br />
+            <br />
+            <label htmlFor='password'>Password:</label> <br />
+            <input type='password' /> <br />
+            <button className='btn'>sign in</button>
+          </form>
+        </div>
+        <div className='auth-container__main__sign-up'>
+          <h2>Sign Up</h2>
+          <p>
+            We NEVER save credit card info.
+            Registering with us makes checkout easier and faster and enables us
+            recommend better products based on your taste
+          </p>
+          <form className='auth-container__form-control'>
+            <label htmlFor='email'>Email:</label> <br />
+            <input type='text' name='email' /> 
+            <br />
+            <br />
+            <label htmlFor='password'>Password:</label> <br />
+            <input type='password' /> <br />
+            <button className='btn'>sign in</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
