543d83fd214a155f87d55bec174b61779b9caa24
Arinze <arinze19@outlook.com>
2022-03-21 17:54:08 +0100

    minor-fix

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create()
 
-api.defaults.baseURL = 'http://localhost:5151/api/v1'
-
-// 'https://react-project-4-api.herokuapp.com/api/v1';
+api.defaults.baseURL = 'https://react-project-4-api.herokuapp.com/api/v1';
 api.defaults.timeout = 8000
 
 export default api;
