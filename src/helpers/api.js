da878f55ef5d54f9206d7294ba59333e8bf735c7
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-07-24 17:25:19 +0100

    changes

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create()
 
-api.defaults.baseURL = 'https://react-project-4-api.herokuapp.com/api/v1';
-// api.defaults.timeout = 8000
+api.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 export default api;
